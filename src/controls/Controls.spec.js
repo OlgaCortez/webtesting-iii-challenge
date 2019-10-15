66e396a51a63b5f2499282f0a2fc9c442788dcf0
Olga Cortez <olgalataviacortez@gmail.com>
2019-10-14 20:58:57 -0500

    All test are passing met MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,38 @@
 // Test away!
+import React from 'react';
+import {render, fireEvent} from 'react-testing-library';
+import Controls from './Controls';
+
+describe ('<Controls />', () => {
+    it("open and unlocked", () => {
+        const mock = jest.fn();
+        const {queryByText} = render(<Controls closed={false} locked={false} toggleClosed={mock} />)
+        const lockedButton = queryByText("Lock Gate");
+        expect(lockedButton.disabled).toBe(true)
+        const closeButton = queryByText("Close Gate");
+        expect(closeButton.disabled).toBe(false)
+        fireEvent.click(closeButton);
+        expect(mock).toBeCalled()
+})
+    it("closed and unlock", () => {
+        const mock = jest.fn();
+        const {queryByText} = render(<Controls locked={false} closed={true} toggleClosed={mock} />)
+        const lockedButton = queryByText("Lock Gate");
+        expect(lockedButton.disabled).toBe(true)
+        const openButton = queryByText("Open Gate");
+        expect(openButton.disabled).toBe(false)
+        fireEvent.click(openButton);
+        expect(mock).toBeCalled()
+  })
+    it("closed and locked", () => {
+        const mock = jest.fn();
+        const {queryByText} = render(<Controls locked={true} closed={true} toggleLocked={mock} />)
+        const unlockedButton = queryByText("Unlock Gate");
+        expect(unlockedButton.disabled).toBe(false)
+        const openButton = queryByText("Open Gate");
+        expect(openButton.disabled).toBe(false)
+        fireEvent.click(unlockedButton);
+        expect(mock).toBeCalled()
+  })
+
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,17 @@
 // Test away!
+import React from "react"
+import { render } from "react-testing-library"
+import Display from "./Display";
+
+describe("<Display />", () => {
+  it("Shows state (unlocked, open) ", () => {
+    const { getByText } = render(<Display />);
+    expect(getByText("Unlocked"));
+    expect(getByText("Open"));
+  })
+  it("Changes state through props", () => {
+    const { getByText } = render(<Display locked={true} closed={true} />);
+    expect(getByText("Locked"));
+    expect(getByText("Closed"));
+  })
+})
